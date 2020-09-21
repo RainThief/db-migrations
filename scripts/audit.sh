47efec3ce2a051cc4db5cdada8ea401fbbdd4aac
Martin Fleming <martinfleming@live.co.uk>
2020-09-21 21:54:04 +0000

    cleanup uneeded logic in license checker

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -9,22 +9,16 @@ exitonfail $? "dependency audit"
 LICENSE_LIST="$(curl -L https://raw.githubusercontent.com/defencedigital/react-lint-config/non-spdx-licenses/licenses.json | jq -c -r '.[]')"
 exitonfail $? "obtaining license list"
 
-ALLOWED_LICENSES=""
-IFS=$'\n'
-for LICENSE in $LICENSE_LIST; do
-    ALLOWED_LICENSES="${ALLOWED_LICENSES}${LICENSE};"
-done
-
 LICENSES_USED="$(pip-licenses)"
 
 # check what licenses are used in pip packages and remove ones that are allowed
-IFS=";"
-for LICENSE in $ALLOWED_LICENSES; do
+IFS=$'\n'
+for LICENSE in $LICENSE_LIST; do
     LICENSES_USED="$(sed "/$LICENSE/d" <<< "$LICENSES_USED")"
 done
 
 # if results contain more than just heading row we have license violations
-# disable word splitting wanrning as wc produces number
+# disable word splitting warning as wc produces number
 # shellcheck disable=SC2046
 if [ $(echo "$LICENSES_USED" | wc -l) -gt 1 ]; then
     echo "$LICENSES_USED"
